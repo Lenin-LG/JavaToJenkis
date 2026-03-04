21acc0241d9573640c58d702e53af9e0a8232675
Lenin-LG <lauragarcialenin@gmail.com>
2026-03-04 12:06:17 -0500

    testv9

--- src/main/java/com/bootcamp/transactions/application/service/dto/ChargeRequest.java
+++ src/main/java/com/bootcamp/transactions/application/service/dto/ChargeRequest.java
@@ -2,6 +2,9 @@ package com.bootcamp.transactions.application.service.dto;
 
 import java.math.BigDecimal;
 
+/*
+    * DTO para la solicitud de un cargo en una cuenta.
+ */
 public record ChargeRequest(
         String accountId,
         BigDecimal amount,
