1f0eb90e822233bfcfe6d6a7927dac37d9af4741
Lenin-LG <lauragarcialenin@gmail.com>
2026-03-04 17:50:44 -0500

    testv17

--- src/main/java/com/bootcamp/transactions/domain/exception/NotFoundException.java
+++ src/main/java/com/bootcamp/transactions/domain/exception/NotFoundException.java
@@ -1,5 +1,8 @@
 package com.bootcamp.transactions.domain.exception;
 
+/*
+    * Excepción personalizada para indicar que un recurso no fue encontrado.
+ */
 public class NotFoundException extends RuntimeException {
     public NotFoundException(String message) {
         super(message);
