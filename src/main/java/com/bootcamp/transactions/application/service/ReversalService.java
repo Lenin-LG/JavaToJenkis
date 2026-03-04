7f43f64bd7bceb14e00debb863cc0bf2e817e8d4
Lenin-LG <lauragarcialenin@gmail.com>
2026-03-04 11:40:36 -0500

    testv7

--- src/main/java/com/bootcamp/transactions/application/service/ReversalService.java
+++ src/main/java/com/bootcamp/transactions/application/service/ReversalService.java
@@ -19,6 +19,17 @@ public class ReversalService implements ReversalUseCase {
     private final OperationRepositoryPort operationRepository;
     private final AccountRepositoryPort accountRepository;
 
+    /*
+        * Para reversar una operación, seguimos estos pasos:
+        * 1. Buscar la operación original por su ID, si no existe lanzar excepción
+        * 2. Verificar que la operación no sea una reversa, si es así lanzar excepción
+        * 3. Verificar que la operación no haya sido ya reversada, si es así lanzar excepción
+        * 4. Buscar la cuenta asociada a la operación original, si no existe lanzar excepción
+        * 5. Aplicar la reversa a la cuenta (sumar el monto)
+        * 6. Crear una nueva operación de tipo REVERSAL con estado COMPLETED y referencia a la operación original
+        * 7. Marcar la operación original como REVERSED
+        * 8. Guardar la cuenta actualizada, la operación original actualizada y la nueva operación de reversa en la base de datos
+     */
     @Override
     @Transactional
     public Mono<OperationResponse> reverse(String operationId) {
